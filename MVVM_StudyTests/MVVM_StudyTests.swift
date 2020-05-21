7169b0d491c3f18a9d86a1d4cc8bb2eecd06f8ad
mkilmer <marcoskilmer1@gmail.com>
2020-05-21 09:09:23 -0300

    Initial Commit

---
+++ MVVM_StudyTests/MVVM_StudyTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVM_StudyTests.swift
+//  MVVM_StudyTests
+//
+//  Created by Marcos Kilmer on 21/05/20.
+//  Copyright © 2020 mkilmer. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_Study
+
+class MVVM_StudyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
